51e331c42a1912072d972967f6d4d7d8e887d2cf
Ethan Glover <hello@ethang.email>
2023-07-23 15:09:07 -0500

    were supposed to be nuking

--- docker-reset.ps1
+++ docker-reset.ps1
@@ -2,4 +2,4 @@ docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 docker volume rm $(docker volume ls -q)
-docker network prune -f
+docker network rm $(docker network ls -q)
