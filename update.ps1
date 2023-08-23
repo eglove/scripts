78420647f0e1308ca0a64bb48bab066333db7436
Ethan Glover <hello@ethang.email>
2023-08-23 11:27:04 -0500

    "Add update commands for Visual Studio and Node in update.ps1
    Enhancements were made to update.ps1 to include commands for updating Visual Studio and Node. This offers a smoother and more comprehensive update process. Intended to save time and effort, reducing manual update tasks."

--- update.ps1
+++ update.ps1
@@ -1,4 +1,11 @@
+# Visual Studio Update
+dotnet tool update -g dotnet-vs
+vs update --all
+
+# Update software
 choco upgrade all
+
+# Node Update
 nvm install latest
 nvm use latest
 npm i -g @sanity/cli empty-trash-cli jest npm nx pnpm trash-cli turbo yarn typescript
