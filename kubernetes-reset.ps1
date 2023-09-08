dab8d6e531e8b70d723f8371c97e01e178259204
Ethan Glover <hello@ethang.email>
2023-09-08 17:36:07 -0500

    Add script for complete Kubernetes reset
    Added a new script 'kubernetes-reset.ps1' to delete all Kubernetes components at once. This script removes deployments, statefulsets, services, configmaps, secrets, and PVCs. It's useful for completely resetting the Kubernetes environment either for commencing a fresh project or after the conclusion of a current one.

---
+++ kubernetes-reset.ps1
@@ -0,0 +1,7 @@
+kubectl delete deployment --all
+kubectl delete statefulset --all
+kubectl delete svc --all
+kubectl delete configmap --all
+kubectl delete secret --all
+kubectl delete pvc --all
+kubectl delete namespace --all
