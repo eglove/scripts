418a61b58c60f79d45259fb0ba209e8d7d2311cb
Ethan <hello@ethang.email>
2024-08-06 18:56:10 -0500

    clean docker on shutdown

--- goodnight.ps1
+++ goodnight.ps1
@@ -2,6 +2,6 @@ node C:\Users\hello\Projects\ethang\eglove\git-history.mjs
 Write-Host "Updating Windows..."
 Update-Module PSWindowsUpdate
 Get-WindowsUpdate -MicrosoftUpdate -AcceptAll -Install
-#docker system prune -af
+docker system prune -af
 update
 Stop-Computer
