02e21e4a223e43acbbc3731686c5d18adb3c6e83
Ethan <hello@ethang.email>
2024-08-11 15:19:12 -0500

    Add prisma and nextui globals

--- update-node.ps1
+++ update-node.ps1
@@ -1,5 +1,5 @@
 $currentLocation = Get-Location
 Set-Location ~/projects/scripts
 node update-node.mjs
-npm i -g npm pnpm yarn typescript
+npm i -g npm pnpm yarn typescript prisma nextui-cli
 Set-Location $currentLocation
