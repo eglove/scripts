2ce24775f527391f5bb5e64cb27c61da30be4ea5
Ethan <hello@ethang.email>
2024-09-27 22:21:36 -0500

    add bun to globals

--- update-node.ps1
+++ update-node.ps1
@@ -1,5 +1,5 @@
 $currentLocation = Get-Location
 Set-Location ~/projects/scripts
 node update-node.mjs
-npm i -g npm pnpm yarn typescript prisma nextui-cli
+npm i -g npm pnpm yarn typescript prisma nextui-cli bun
 Set-Location $currentLocation
