b42c6640155e8eb65a0601a624bf1487eb408c17
Ethan <hello@ethang.email>
2024-09-27 22:07:34 -0500

    move to bun

--- build.mjs
+++ build.mjs
@@ -1,6 +1,6 @@
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("scripts", "master", {
-  scripts: ["UPDATE", "DEDUPE", "LINT"],
+  scripts: ["bun x taze latest -I", "bun lint"],
   isLibrary: false,
 });
